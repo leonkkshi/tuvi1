9b05659b14510f79a0b062be5388cea38fb3caa3
tu2005tktn <tunguyen209205@gmail.com>
2026-01-05 11:10:36 +0700

    thay model

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a124599f4e529147021f4cd25ffcf1f28dbe680")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f91318a1a79e4b770955bfcb5d9406b21d22256f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
